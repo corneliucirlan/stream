edfe82546b7aa1505ba91683757673c08d628974
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2023-01-09 10:20:55 +0000

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,107 +1,99 @@
 import { useEffect, useState } from "react"
 
-export const getStaticProps = async () => {
+import MovieCard from "../components/movie-card"
+import { getPhotoID, searchQuery, getAllCountries, getAllAvailableProviders } from "../utils/justwatch"
 
-	// Get all availeble countries
-	const response = await fetch(`https://apis.justwatch.com/content/locales/state`)
-	let countries = await response.json()
+// Get list of all countries
+export const getStaticProps = async () => ({ props: { countries: await getAllCountries() } })
+
+export default ({ countries }) => {
 	
-	// Get all available providers for every country
-	let providers = {}
-	await Promise.all(countries.map(async (country) => {
-		let response = await fetch(`https://apis.justwatch.com/content/providers/locale/${country.full_locale}`)
-		providers[country.full_locale] = await response.json()
-	}))
+	// console.log("All countries: ", countries)
 
-	return {
-		props: {
-			countries: countries,
-			providers: providers
-		}
-	}
-}
+	// Store search locale / country
+	let [ searchLocale, setSearchLocale ] = useState('en_US')
 
-export default ({ countries, providers }) => {
-
-	// console.log("Countries", countries)
-	// console.log("Providers", providers)
-
+	// Store search box state
 	let [ searchInput, setSearchInput ] = useState('')
 
+	// Store search results
+	let [ searchResults, setSearchResults ] = useState([])
+
 	useEffect(() => {
-		// console.log("Search: ", searchInput)
-
-		// Search movie or tv show
-		const search = async (query, locale) => {
-
-			// Fetch URL
-			const url = `/api/content/titles/${locale}/popular`
-
-			// Query body
-			const body = {
-				"query": query,
-			}
-
-			// Fetch results
-			const response = await fetch(url, {
-				method: "POST",
-				body: JSON.stringify(body),
-				headers: {
-					'Content-Type': 'application/json',
-					"X-Requested-With": "fetch"
-				}
-			})
-
-			// Return results as JSON object
-			return await response.json()
-		}
-
-		const getAllOffers = async (id, type) => {
-
-			const whereToStream = []
-
-			// Loop through all countries
-			await Promise.all(countries.map(async (country) => {
-
-				// Create movie/tv show URL for specific country
-				const url = `/api/content/titles/${type}/${id}/locale/${country.full_locale}`
-
-				// Fetch offers from the country
-				const response = await fetch(url)
-
-				// Get offers as JSON object
-				const stream = await response.json()
-
-				// Add offers to whereToStream if available
-				stream.offers && whereToStream.push(stream)
-			}))
-
-			return whereToStream
-		}
 
 		// Search Movie or TV Show
-		// search("black adam", 'en_US').then(response => {
-		searchInput && search(searchInput, 'en_US').then(response => {
-			// console.log(response.items[0])
+		// searchInput && searchQuery(searchInput, searchLocale).then(response => {
+		searchQuery("black widow", searchLocale).then(response => {
+			console.log(response.items)
 
-			let id = response.items[0].id
-			let type = response.items[0].object_type
+			const items = response.items.map(item => {
+				return ({
+					id: item.id,
+					title: item.title,
+					poster: `https://images.justwatch.com/poster/${getPhotoID(item.poster)}/s592/poster.webp`,
+					type: item.object_type,
+					releasseYear: item.original_release_year
+				})
+			})
 
-			getAllOffers(id, type).then(response => console.log(response))
+			setSearchResults(items)
 		})
 
 	}, [ searchInput ])
 
+	// Save search country
+	const handleLocaleChange = event => setSearchLocale(event.target.value)
+
+	useEffect(() =>{
+		console.log("LOCALE: ", searchLocale)
+	}, [searchLocale])
+
 	// Save search input
-	const handleOnChange = event => setSearchInput(event.target.value)
+	const handleSearchChange = event => setSearchInput(event.target.value)
 
 	return (
-		<div>
-			<input
-				type='text'
-				onChange={handleOnChange}
-				value={searchInput}
-			/>
+		<div className="container">
+
+			<div className="row justify-content-center search-box">
+				<div className="col col-md-3">
+					<select
+						className="form-control"
+						onChange={handleLocaleChange}
+					>
+						{countries.map(country =>
+							<option
+								value={country.full_locale}
+							>
+								{country.country}
+							</option>
+						)}
+					</select>
+				</div>
+
+				<div className="col col-md-6">
+					<input
+						type="text"
+						className="form-control"
+						onChange={handleSearchChange}
+						value={searchInput}
+						placeholder='Search for a movie or tv show'
+						autoFocus={true}
+					/>
+				</div>
+			</div>
+
+			<div className="row">
+				{searchResults && searchResults.map(result =>
+					<MovieCard
+						key={result.id}
+						id={result.id}
+						title={result.title}
+						type={result.type}
+						poster={result.poster}
+					/>
+				)}
+			</div>
+
 		</div>
 	)
 }
