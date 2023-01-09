82f0d477b904fe57d2488de26c05db1ef1051d98
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2023-01-09 15:24:32 +0000

    Get all streaming services available for a movie

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -15,33 +15,32 @@ export const getStaticProps = async (context) => ({
 })
 
 export default ({ movie, movieProviders, allProviders }) => {
-
+	
+	// Loading the movie
 	if (!movie) return <div>Loading</div>
-	// console.log("Movie data: ", movie)
-	// console.log("Movie providers: ", movieProviders)
-	console.log("All providers: ", allProviders)
 
 	// Get all providers for the movie
-	let data = movieProviders.map(provider => {
-console.log(provider)
+		let data = movieProviders.map(provider => {
+
 		// Get offers by monetization type
 		const getOffersByType = (providerOffers, monetization) => {
 
 			// Filter and map offers in a single step
-			const offers = providerOffers
+			let offers = providerOffers
 				.filter(offer => offer.monetization_type === monetization)
 				.map(({ provider_id, package_short_name, presentation_type, monetization_type }) => ({
 					id: provider_id,
 					shortName: package_short_name,
-					name: allProviders[provider_id].clear_name,
-					icon: `https://images.justwatch.com/icon/${getPhotoID(allProviders[provider_id].icon_url)}/s100`,
+					name: typeof allProviders[provider_id] !== 'undefined' && allProviders[provider_id].clear_name,
+					// name: allProviders[provider_id].clear_name,
+					icon: typeof allProviders[provider_id] !== 'undefined' ? `https://images.justwatch.com/icon/${getPhotoID(allProviders[provider_id].icon_url)}/s100` : null,
 					resolution: presentation_type,
 					type: monetization_type,
 			}))
 
 			// Group offers by id and name
 			const groupedOffers = offers.reduce((groups, { id, shortName, name, icon, resolution, type }) => {
-			
+
 				// Create a group key from id and name
 				const groupKey = `${id}-${name}`
 
@@ -51,24 +50,32 @@ console.log(provider)
 				}
 
 				// Add resolution to group
-				groups[groupKey].resolutions.push(resolution);
+				groups[groupKey].resolutions.push(resolution)
 
-				return groups;
+				return groups
 			}, {})
 
 			// Convert grouped offers object to array
-			return Object.values(groupedOffers)
+			let list = Object.values(groupedOffers)
+
+			// Return array if not empty
+			return list.length > 0 ? list : null
 		}
 
+		// Get offers
 		let flatrate = getOffersByType(provider.offers, 'flatrate')
 		let rent = getOffersByType(provider.offers, 'rent')
 		let buy = getOffersByType(provider.offers, 'buy')
 
 		return {
 			country: provider.name,
-			offers: {
-				flatrate, rent, buy
-			}
+			offers: Object.entries({ flatrate, rent, buy }).reduce(
+				(acc, [key, value]) => {
+					if (value !== null) {
+						acc[key] = value;
+					}
+					return acc;
+			}, {}),
 		}
 	})
 
@@ -84,8 +91,8 @@ console.log(provider)
 				style={{ objectFit: 'contain' }}
 			/> */}
 
-			{/* {movieProviders.map(( provider, key ) =>
-				<h2 key={key}>{provider.name}</h2>
+			{/* {data.map(( provider, key) =>
+				<h1 key={key}>{provider.country}</h1>
 			)} */}
 		</div>
 	)
