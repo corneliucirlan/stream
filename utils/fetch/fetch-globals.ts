9a41ddee1c435ca381244dada6bc0c55722cff5b
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2023-10-25 16:22:08 +0300

    Updated mode

--- utils/fetch/fetch-globals.ts
+++ utils/fetch/fetch-globals.ts
@@ -3,23 +3,13 @@ export const JUSTWATCH_GRAPH_URL = `${JUSTWATCH_BASE_URL}/graphql`
 export const JUSTWATCH_IMAGE_URL = "https://images.justwatch.com"
 
 export const HEADERS = {
-	accept: "*/*",
-	"accept-language": "en-US,en;q=0.9",
-	"app-version": "3.8.0-web",
-	"content-type": "application/json",
-	"device-id": "rsRIG2n_Ee6cUfrEg3aSzA",
-	"sec-ch-ua":
-		'"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
-	"sec-ch-ua-mobile": "?0",
-	"sec-ch-ua-platform": '"Windows"',
-	"sec-fetch-dest": "empty",
-	"sec-fetch-mode": "cors",
-	"sec-fetch-site": "same-site"
+	"Content-Type": "application/json",
+	"X-Requested-With": "fetch"
 }
 
 export const fetchOptions = {
 	method: "POST",
-	mode: "cors" as RequestMode,
+	mode: "no-cors" as RequestMode,
 	credentials: "omit" as RequestCredentials,
 	headers: HEADERS
 }
