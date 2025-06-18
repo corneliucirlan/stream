80c9d5b837f19a756ca1e822aca5e181daee1266
Corneliu Cirlan <corneliu@corneliucirlan.com>
2025-06-18 11:14:18 +0300

    Updated React

--- utils/tmdb/homepage-backdrop.ts
+++ utils/tmdb/homepage-backdrop.ts
@@ -5,7 +5,5 @@ import { createApiRequest } from "./tmdb-api"
 export default async (): Promise<string | undefined> => {
 	const data = await createApiRequest<{ results: any[] }>("/trending/all/day")
 
-	if (!data || !data.results || data.results.length === 0) return undefined
-
-	return data.results[0].backdrop_path
+	return data?.results?.[0]?.backdrop_path
 }
